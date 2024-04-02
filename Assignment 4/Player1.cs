fd539424aae0a1c9d5a150620d2aeeaf35331c33
NotCraz <161373890+NotCraz@users.noreply.github.com>
2024-04-02 16:38:42 -0400

    added player1 function

---
+++ Assignment 4/Player1.cs
@@ -0,0 +1,75 @@
+﻿using System.Numerics;
+using Raylib_cs;
+namespace Assignment_4
+{
+    
+
+    internal class Player1
+    {
+        
+        Vector2 playerposition;
+         Vector2 velocity;
+         Vector2 size;
+        public Color color;
+        private int screenWidth = 800;
+        private int screenHeight = 600;
+
+        //player constructor 
+        public  Player1()
+        {
+            playerposition = new Vector2(25, 300);
+            size = new Vector2(10, 50);
+            velocity.Y = 10;
+            velocity.X = 0;
+            color = Color.Blue;
+
+        }
+
+        //movment 
+        public void Move()
+        {
+            //UP
+            if ( Raylib.IsKeyDown(KeyboardKey.W))
+            {
+                playerposition.Y -= velocity.Y;
+            }
+
+            //DOWN
+            if (Raylib.IsKeyDown(KeyboardKey.S))
+            {
+                playerposition.Y += velocity.Y;
+            }
+        }
+        
+        //render the paddle 
+        public void RenderPlayer()
+        {
+            Raylib.DrawRectangleV(playerposition, size, color);
+        }
+
+        //collision with the walls 
+        public void CheckPlayerWallCollision()
+        {
+            if (playerposition.Y < 0)
+            {
+                playerposition.Y = 0;
+            }
+
+            if (playerposition.Y + size.Y > screenHeight)
+            {
+                playerposition.Y = screenHeight - size.Y;
+            }
+        }
+
+        public void BallCollision()
+        {
+            float leftEdge1 = playerposition.X;
+            float rightEdge1 = playerposition.X + size.X;
+            bool doesOverlapLeft = leftEdge1 < rightEdge2;
+            bool doesOverlapRight = rightEdge1 > leftEdge2;
+
+
+        }
+    }
+    
+}
--- Assignment 4/Program.cs
+++ Assignment 4/Program.cs
@@ -1,10 +1,56 @@
-﻿namespace Assignment_4
+﻿using Raylib_cs;
+using Assignment_4;
+namespace Assignment_4
 {
     internal class Program
     {
+        static string title = "PONG";
+        static Player1 p1;
         static void Main(string[] args)
         {
-            Console.WriteLine("Goodbye, World!");
+            // Create a window to draw to. The arguments define width and height
+            Raylib.InitWindow(800, 600, title);
+            // Set the target frames-per-second (FPS)
+            Raylib.SetTargetFPS(60);
+
+            // Setup your game. This is a function YOU define.
+            Setup();
+
+            // Loop so long as window should not close
+            while (!Raylib.WindowShouldClose())
+            {
+
+                // Enable drawing to the canvas (window)
+                Raylib.BeginDrawing();
+                // Clear the canvas with one color
+                Raylib.ClearBackground(Color.White);
+
+                // Your game code here. This is a function YOU define.
+                Update();
+
+                // Stop drawing to the canvas, begin displaying the frame
+                Raylib.EndDrawing();
+
+            }
+            // Close the window
+            Raylib.CloseWindow();
         }
+
+        static void Setup()
+        {
+            
+            p1 = new Player1();
+            
+        }
+        static void Update()
+        {
+            p1.Move();
+            p1.CheckPlayerWallCollision();
+            p1.RenderPlayer();
+
+
+        }
+
     }
+
 }
