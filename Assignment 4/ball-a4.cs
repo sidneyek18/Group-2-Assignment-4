fefcfd2e260d89e3f3e2dd2e9d0de01c32f31b91
Ameliaa9 <161509242+Ameliaa9@users.noreply.github.com>
2024-04-02 19:22:29 -0400

    ball but no movement yet

---
+++ Assignment 4/ball-a4.cs
@@ -0,0 +1,32 @@
+﻿using Raylib_cs;
+
+namespace ass4_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting the game");
+
+            int screenWidth = 1280;
+            int screenHeight = 800;
+            Raylib.InitWindow(screenWidth, screenHeight, "Pong");
+            Raylib.SetTargetFPS(60);
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Black);
+
+                // Drawing
+
+                Raylib.DrawCircle(screenWidth / 2, screenHeight / 2, 20, Color.White);
+
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
