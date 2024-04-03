c0eda34159309e67a6d72bf9504bb550f69a72d0
Ameliaa9 <161509242+Ameliaa9@users.noreply.github.com>
2024-04-03 16:06:51 -0400

    ball speed

--- Assignment 4/ball-a4.cs
+++ Assignment 4/ball-a4.cs
@@ -1,4 +1,5 @@
 ﻿using Raylib_cs;
+using System;
 
 namespace ass4_practice
 {
@@ -13,15 +14,23 @@ namespace ass4_practice
             Raylib.InitWindow(screenWidth, screenHeight, "Pong");
             Raylib.SetTargetFPS(60);
 
+            int x = screenWidth / 2; // Y position
+            int y = screenHeight / 2; // Y position
+            int radius = 20; // circle radius 
+            int speed_x = 5; // horizontal speed
+            int speed_y = 5; // vertical speed
+
             while (!Raylib.WindowShouldClose())
             {
+                x += speed_x;
+                y += speed_y;
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.Black);
 
                 // Drawing
-
                 Raylib.DrawCircle(screenWidth / 2, screenHeight / 2, 20, Color.White);
 
+                Raylib.DrawCircle(screenWidth / 2, screenHeight / 2, 20, Color.White);
 
                 Raylib.EndDrawing();
             }
